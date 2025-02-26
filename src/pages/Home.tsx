3ccef8f291f97c999313d91c4bab69d4df72d05c
echo <play3step@naver.com>
2025-02-26 15:48:51 +0900

    feat: 파이어베이스 구글 로그인 연결

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,26 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import SocialAuthButton from '../components/common/SocialAuthButton'
+import { authService } from '../firebase'
 
 const Home = () => {
+  const handleGoogleLogin = async () => {
+    try {
+      const provider = new GoogleAuthProvider()
+      const result = await signInWithPopup(authService, provider)
+      const token = await result.user.getIdToken()
+      if (result) {
+        console.log(token)
+        return { user: result.user, token }
+      }
+    } catch (error) {
+      console.error('구글 로그인 중 오류 발생:', error)
+      return null
+    }
+  }
+
   return (
     <h1>
+      <button onClick={handleGoogleLogin}>로그인</button>
       <SocialAuthButton />
     </h1>
   )

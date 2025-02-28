9c8f808f66abc8421ac5e740780c6f6a30cb07d7
echo <play3step@naver.com>
2025-02-28 12:58:08 +0900

    remove: 필요없는 ui 삭제

--- src/components/common/SocialAuthButton.tsx
+++ src/components/common/SocialAuthButton.tsx
@@ -1,14 +1,20 @@
 import gogle from '../../assets/gogle.svg'
 
-const SocialAuthButton = () => {
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode
+}
+
+const SocialAuthButton = ({ children, onClick }: Props) => {
   return (
-    <button className="relative w-[424px] h-16 border border-black rounded flex items-center justify-center transition duration-200 hover:bg-gray-100">
+    <button
+      className="relative w-[424px] h-16 border border-black rounded flex items-center justify-center transition duration-200 hover:bg-gray-100"
+      onClick={onClick}>
       <img
         src={gogle}
         alt="google 로그인"
         className="absolute left-6 w-10"
       />
-      <span className="font-semibold text-medium">Google 로그인</span>
+      <span className="font-semibold text-medium">{children} 로그인</span>
     </button>
   )
 }

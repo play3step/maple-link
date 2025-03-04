ef19350cfa091491cf24d0adc56bae3bd16e3848
echo <play3step@naver.com>
2025-03-04 15:16:07 +0900

    feat: 로그아웃 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,13 +1,22 @@
 import { Link } from 'react-router-dom'
+import { useAuth } from '../../hooks/useAuth'
+import Button from './Button'
 
 function Header() {
+  const { userLogout } = useAuth()
   return (
     <div className="w-full max-w-[1440px] mx-auto px-4 py-5 border-b border-black flex justify-between">
       <Link
         to="/"
         className="md:text-2xl text-lg">
-        Maple Nexus
+        Maple Link
       </Link>
+      <Button
+        size="small"
+        scheme="outlined"
+        onClick={userLogout}>
+        로그아웃
+      </Button>
     </div>
   )
 }

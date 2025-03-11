100e23fe33c3711ad45bdad8b63b07f5645b203b
echo <play3step@naver.com>
2025-03-11 17:29:33 +0900

    Style: navbar 구성

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,23 +1,56 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../../hooks/useAuth'
 import Button from './Button'
 
 function Header() {
   const { userLogout } = useAuth()
+  const nav = useNavigate()
   return (
-    <div className="w-full max-w-[1440px] mx-auto px-4 py-5 border-b border-black flex justify-between">
-      <Link
-        to="/"
-        className="md:text-2xl text-lg">
-        Maple Link
-      </Link>
+    <header className="w-full max-w-[1440px] mx-auto px-4 py-5 border-b border-black flex justify-between items-center">
+      <div className="flex items-center">
+        <Link
+          to="/"
+          className="md:text-2xl text-lg">
+          Maple Link
+        </Link>
+        <nav className="flex items-center gap-8 ml-10">
+          <Button
+            scheme="subtle"
+            size="small"
+            onClick={() => nav('/character')}>
+            캐릭터정보
+          </Button>
+          <Button
+            scheme="subtle"
+            size="small"
+            onClick={() => nav('/guild')}>
+            길드관리
+          </Button>
+          <Button
+            scheme="subtle"
+            size="small">
+            캘린더
+          </Button>
+          <Button
+            scheme="subtle"
+            size="small">
+            회의
+          </Button>
+          <Button
+            scheme="subtle"
+            size="small">
+            공지사항
+          </Button>
+        </nav>
+      </div>
+
       <Button
         size="small"
         scheme="outlined"
         onClick={userLogout}>
         로그아웃
       </Button>
-    </div>
+    </header>
   )
 }
 

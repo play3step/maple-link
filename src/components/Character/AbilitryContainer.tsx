65b69283eb309c732e4269030c3b0640c4078052
echo <play3step@naver.com>
2025-03-14 15:41:10 +0900

    Feat: 어빌리티 데이터 출력

--- src/components/Character/AbilitryContainer.tsx
+++ src/components/Character/AbilitryContainer.tsx
@@ -7,29 +7,38 @@ interface Props {
 
 export const AbilitryContainer = ({ ability }: Props) => {
   const [selected, setSelected] = useState(1)
-
+  const selectedPreset =
+    selected === 1
+      ? ability.ability_preset_1
+      : selected === 2
+        ? ability.ability_preset_2
+        : ability.ability_preset_3
   return (
-    <div className="w-[316px] h-[174px] border border-black rounded-lg flex flex-col justify-center ">
-      <p>ABILITY</p>
-      <div className="flex gap-4">
-        <button onClick={() => setSelected(1)}>1</button>
-        <button onClick={() => setSelected(2)}>2</button>
-        <button onClick={() => setSelected(3)}>3</button>
+    <div className="w-[316px] h-[174px] border border-black rounded-lg flex flex-col justify-center">
+      <p className="mb-2 text-center font-bold">ABILITY</p>
+      <div className="flex justify-center gap-4 mb-3">
+        {[1, 2, 3].map(num => (
+          <button
+            key={num}
+            onClick={() => setSelected(num)}
+            className={`px-2 py-1 rounded ${
+              selected === num
+                ? 'bg-primary text-white'
+                : 'bg-gray-200 text-black'
+            }`}>
+            {num}
+          </button>
+        ))}
       </div>
-
-      {selected === 1 ? (
-        <div>
-          <p>{ability.ability_preset_1.ability_info[0].ability_value}</p>
-        </div>
-      ) : selected === 2 ? (
-        <div>
-          <p>{ability.ability_preset_2.ability_info[0].ability_value}</p>
-        </div>
-      ) : (
-        <div>
-          <p>{ability.ability_preset_3.ability_info[0].ability_value}</p>
-        </div>
-      )}
+      <div className="flex flex-col gap-1 p-2.5">
+        {selectedPreset.ability_info.map((item, index) => (
+          <p
+            key={index}
+            className="text-sm">
+            {item.ability_value}
+          </p>
+        ))}
+      </div>
     </div>
   )
 }

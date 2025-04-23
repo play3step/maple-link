f26c0d19dcebef7a3c34ad5e4dd26c607a6b90d3
echo <play3step@naver.com>
2025-04-23 15:21:13 +0900

    stlye : 잠재옵션 스타일 및 출력

--- src/components/Character/Inventory/Slot.tsx
+++ src/components/Character/Inventory/Slot.tsx
@@ -7,13 +7,14 @@ interface Prop {
 
 export const Slot = ({ item, onClick }: Prop) => {
   return (
-    <div className="w-12 h-12 p-1 bg-gray-200 border-gray-400 rounded-md overflow-hidden hover:border-red-600 border-2">
+    <div
+      className="w-12 h-12 p-1 bg-gray-200 border-gray-400 rounded-md overflow-hidden hover:border-red-600 border-2"
+      onClick={() => onClick(item)}>
       {item?.item_icon && (
         <img
           src={item.item_icon}
           alt={item.item_equipment_slot}
           className="w-full h-full object-contain"
-          onClick={() => onClick(item)}
         />
       )}
     </div>

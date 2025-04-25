48d5b9fddf8fb147be56f192d0501703dc172ad3
echo <play3step@naver.com>
2025-04-25 15:48:54 +0900

    style: 일정 출력 크기 조절

--- src/components/modal/EventListModal.tsx
+++ src/components/modal/EventListModal.tsx
@@ -3,6 +3,8 @@ import Button from '../common/Button'
 import InputText from '../common/InputText'
 import Title from '../common/Title'
 import ModalLayout from './ModalLayout'
+import nexonIcon from '../../assets/nexon.webp'
+import noteIcon from '../../assets/note.png'
 
 interface Props {
   list: CalendarType[]
@@ -13,10 +15,42 @@ export const EventListModal = ({ selectedDate, list }: Props) => {
   return (
     <ModalLayout size="medium">
       <div className="h-full flex items-center justify-between flex-col gap-2">
-        <Title size="medium">{selectedDate}</Title>
-        <div>
+        <Title size="medium">{selectedDate} 일정</Title>
+        <div className=" w-full flex flex-col gap-3 text-smal items-center">
           {list.length !== 0 ? (
-            list.map(v => <div>{v.title}</div>)
+            list.map(v =>
+              v.type === 'nexon' ? (
+                <div
+                  className=" w-1/2 flex gap-4 items-center p-2 rounded-md text-white min-h-[48px]"
+                  style={{ backgroundColor: v.color }}>
+                  <img
+                    src={nexonIcon}
+                    className="w-8"
+                  />
+                  <p
+                    className="flex-1 text-sm truncate hover:text-black cursor-pointer"
+                    onClick={() =>
+                      v.nexonUrl && window.open(v.nexonUrl, '_blank')
+                    }>
+                    {v.title}
+                  </p>
+                </div>
+              ) : (
+                <div className="flex gap-4 items-center">
+                  <img
+                    src={noteIcon}
+                    className="w-8"
+                  />
+                  <p
+                    className="flex-1 text-sm truncate hover:text-black cursor-pointer"
+                    onClick={() =>
+                      v.nexonUrl && window.open(v.nexonUrl, '_blank')
+                    }>
+                    {v.title}
+                  </p>
+                </div>
+              )
+            )
           ) : (
             <div>기록된 일정이 없습니다.</div>
           )}

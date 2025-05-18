3e4b322a9647baf5e017bdf77a9fc99dc113925f
echo <play3step@naver.com>
2025-05-18 16:48:19 +0900

    fix: rename all folders to lowercase

--- src/hooks/guild/useGuildsList.ts
+++ src/hooks/guild/useGuildsList.ts
@@ -24,7 +24,7 @@ export const useGuildsList = () => {
 
         if (searchRoom.subGuild && searchRoom.subGuild.subGuildIds.length > 0) {
           const subGuilds = searchRoom.subGuild.subGuildIds.map(
-            (id: number, index: number) => ({
+            (id, index) => ({
               guildId: id,
               guildName: searchRoom.subGuild.names[index]
             })

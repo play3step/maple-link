3081a1a664385cce9ffacf31939d457e21539c42
echo <play3step@naver.com>
2025-06-18 15:14:11 +0900

    feat : 로고 클릭시 로그아웃

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../../hooks/useAuth'
 import Button from './Button'
 import Logo from '../../assets/logo.png'
@@ -22,10 +22,8 @@ function Header() {
   return (
     <header className="w-full max-w-[1440px] mx-auto px-6 py-4 border-b border-blue-100 backdrop-blur-sm bg-white/80 flex justify-between items-center sticky top-0 z-50 shadow-sm">
       <div className="flex items-center">
-        <Link
-          to={
-            userType === 'search' || userType === undefined ? '/' : '/character'
-          }
+        <button
+          onClick={userLogout}
           className="flex items-center gap-2 hover:opacity-80 transition-all">
           <img
             src={Logo}
@@ -35,7 +33,7 @@ function Header() {
           <span className="text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-indigo-600">
             Maple Link
           </span>
-        </Link>
+        </button>
 
         {/* Desktop Navigation */}
         <nav className="hidden md:flex items-center gap-5 ml-10">
